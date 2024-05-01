976ec4cb62a6f8443d21f38508b7b97b46048d42
skuzmenk <kuzmenko.oleksandr1123@vu.cdu.edu.ua>
2024-05-01 10:24:27 +0300

    commit4

--- main1/Task2.cs
+++ main1/Task2.cs
@@ -1,62 +1,91 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace main1
 {
     public class Task2
     {
-        
-            public static void Calculation(ref int n, ref int[][] arr)
+        public static void DifSum(ref int n)
+        {
+            int max = 0;
+            for (int i = 1; i <= n; i++)
             {
-                for (int i = 1; i <= n; i++)
+                int s = 0;
+                if (i >= 10)
                 {
-                    int s = 0;
-                    if (i >= 10)
+                    for (int k = i; k > 0; k /= 10)
                     {
-                        for (int k = i; k > 0; k /= 10)
-                        {
-                            s += k % 10;
-                        }
+                        s += k % 10;
                     }
-                    else
-                    {
-                        s = i;
-                    }
+                }
+                else
+                {
+                    max = i;
+                }
+                if (s > max)
+                {
+                    max = s;
+                }
+            }
+            n = max;
+        }
+
+        public static void Calculation( int n, ref int[][] arr)
+            {
+                for (int i = 1; i <= arr.GetLength(0); i++)
+                {
+                    int s = i;
                     int a = 0;
                     int count = 0;
                     arr[i - 1] = new int[n];
                     int j = 1;
-                    for (; j <= n; j++)
-                    {
-                        if (j % s == 0)
-                        {
-                            arr[i - 1][a] = j;
-                            count++;
-                            a++;
-                        }
-                    }
+                     for (; j <= n; j++)
+                     {
+                         if (j % s == 0)
+                            {
+                        arr[i - 1][a] = j;
+                        count++;
+                        a++;
+                         }
+                     }
                     Array.Resize(ref arr[i - 1], count);
                 }
             }
             public static void Output(int n, int[][] arr)
             {
             Console.WriteLine("The result");
-                for (int i = 0; i < n; i++)
+            for (int i = 1; i <= n; i++)
+            {
+                int s = 0;
+                if (i >= 10)
                 {
-                    for (int j = 0; j < arr[i].Length; j++)
+                    for (int k = i; k > 0; k /= 10)
                     {
-                        Console.Write(arr[i][j] + " ");
+                        s += k % 10;
                     }
-                    Console.WriteLine();
                 }
+                else
+                {
+                    s = i;
+                }
+                for (int j = 0; j < arr[s-1].Length; j++)
+                {
+                    Console.Write(arr[s - 1][j]+" ");
+                }
+                Console.WriteLine() ;
+            }
             Console.WriteLine("Press enter");
         }
         public void Main()
             {
             Console.WriteLine("Enter your number");
                 int n = Convert.ToInt32(Console.ReadLine());
-                int[][] arr = new int[n][];
-                Calculation(ref n, ref arr);
-                Output(n, arr);
+            int l = n;
+            DifSum(ref n);
+            Console.WriteLine(n);
+            int[][] arr = new int[n][];
+            Calculation(l, ref arr);
+                Output(l, arr);
                 Console.ReadLine();
             }
     }
